cb2b620ad814ee2dcc15a1dfe0a3d18c94c12051
Andrej Amelyanovich <amelyanovich.andrej@mail.ru>
2023-04-30 03:57:14 +0300

    feat(Service single page): add adaptive

--- src/pages/ServiceSingle/Content/LeftSideBar/styles.ts
+++ src/pages/ServiceSingle/Content/LeftSideBar/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+import { size } from '@/types/constants';
+
+const { tablet } = size;
+
 export const LeftSidebar = styled.div`
     width: ${({ theme: { width } }) => width.xxl}vw;
     max-width: ${({ theme: { width } }) => width.x3l}px;
@@ -8,6 +12,11 @@ export const LeftSidebar = styled.div`
     display: flex;
     flex-direction: column;
     gap: ${({ theme: { gap } }) => gap.xxxl}px;
+    padding: ${({ theme: { padding } }) => padding.s3s};
+
+    @media (max-width: ${tablet}px) {
+        gap: ${({ theme: { gap } }) => gap.sl}px;
+    }
 `;
 export const TextWithImage = styled.div`
     display: flex;
@@ -34,6 +43,16 @@ export const ContentText = styled.div`
         line-height: ${({ theme: { lineHeight } }) => lineHeight.x2l}px;
         letter-spacing: ${({ theme: { letterSpacing } }) => letterSpacing.x}em;
         color: ${({ theme: { colors } }) => colors.black};
+
+        @media (max-width: ${tablet}px) {
+            font-size: ${({ theme: { fontSize } }) => fontSize.l}px;
+            line-height: ${({ theme: { lineHeight } }) => lineHeight.l}px;
+        }
+    }
+
+    @media (max-width: ${tablet}px) {
+        font-size: ${({ theme: { fontSize } }) => fontSize.l}px;
+        line-height: ${({ theme: { lineHeight } }) => lineHeight.l}px;
     }
 `;
 
@@ -43,6 +62,13 @@ export const SolutionCheckListContainer = styled.div`
 
     display: flex;
     gap: ${({ theme: { gap } }) => gap.xxxl}px;
+
+    @media (max-width: ${tablet}px) {
+        gap: ${({ theme: { gap } }) => gap.sl}px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 `;
 
 export const CheckList = styled.div`
--- src/pages/ServiceSingle/Content/styles.ts
+++ src/pages/ServiceSingle/Content/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
+import { size } from '@/types/constants';
+
+const { laptopM } = size;
 export const SinglePageContentContainer = styled.div`
     width: ${({ theme: { width } }) => width.xxl}%;
     max-width: ${({ theme: { width } }) => width.x5l}px;
@@ -9,6 +12,7 @@ export const SinglePageContentContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    padding: ${({ theme: { padding } }) => padding.s3s};
 `;
 
 export const SinglePageContent = styled.div`
@@ -21,4 +25,11 @@ export const SinglePageContent = styled.div`
     align-items: flex-start;
     gap: ${({ theme: { gap } }) => gap.sl}px;
     margin: ${({ theme: { margin } }) => margin.xxl};
+
+    @media (max-width: ${laptopM}px) {
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: ${({ theme: { margin } }) => margin.l1l};
+    }
 `;
